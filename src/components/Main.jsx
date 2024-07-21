56a25b7e2896b2bef4e39a92bd69b7d9402114d2
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-07-21 20:25:41 +0530

    initial commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-const Main = () => {
+function Main() {
 
     const [todo, setTodo] = useState("");
     const [todos, setTodos] = useState([]);
+    const [showFinished, setShowFinished] = useState(false);
 
     useEffect(() => {
         const storedTodos = JSON.parse(localStorage.getItem("Todos"));
@@ -32,10 +33,6 @@ const Main = () => {
         setTodos(updatedTodos);
     }
 
-    function saveToLocalStorage() {
-        localStorage.setItem("Todos", JSON.stringify(todos));
-    }
-
     function handleEdit(id) {
         const t = todos.find(i => i.id === id);
         if (t) {
@@ -49,13 +46,17 @@ const Main = () => {
         setTodos(todos.filter(id => id.id !== todoId));
     }
 
+    function handleShowFinishedChange() {
+        setShowFinished(!showFinished);
+    }
+
     const handleChange = (e) => setTodo(e.target.value);
 
     return (
         <main className="grid place-items-center">
             <div className="w-10/12 p-5 rounded-xl bg-slate-200">
-                <h3 className="text-xl font-semibold text-center mb-3">
-                    iTask - Manage your todos at one place
+                <h3 className="text-2xl font-semibold text-center mb-3">
+                    Manage your todos at one place
                 </h3>
                 <div className="font-semibold text-lg">Add a Todo</div>
                 <div className="submitInput flex my-2">
@@ -76,33 +77,37 @@ const Main = () => {
                         name="checkbox"
                         className="mr-1"
                         id="showFinished"
+                        checked={showFinished}
+                        onChange={handleShowFinishedChange}
                     /> Show Finished
                 </div>
                 <div className="h-px bg-black opacity-25 w-11/12 mx-auto my-4"></div>
                 <h3 className="text-xl font-semibold">Todos</h3>
                 <div className="todos">
                     {!todos.length && <div>No Todos to display</div>}
-                    {todos.map(item => {
-                        return <div className="todo flex items-center justify-between mb-3" key={item.id}>
-                            <div className="flex">
-                                <input
-                                    id="checkbox"
-                                    name={item.id}
-                                    type="checkbox"
-                                    className="mr-4"
-                                    onChange={handleCheckBox}
-                                    checked={item.isCompleted}
-                                />
-                                <div className={item.isCompleted ? "line-through" : ""}>
-                                    {item.todo}
+                    {todos
+                        .filter(item => showFinished ? item.isCompleted : !item.isCompleted)
+                        .map(item => {
+                            return <div className="todo flex items-center justify-between mb-3" key={item.id}>
+                                <div className="flex">
+                                    <input
+                                        id="checkbox"
+                                        name={item.id}
+                                        type="checkbox"
+                                        className="mr-4"
+                                        onChange={handleCheckBox}
+                                        checked={item.isCompleted}
+                                    />
+                                    <div className={item.isCompleted ? "line-through" : ""}>
+                                        {item.todo}
+                                    </div>
+                                </div>
+                                <div className="buttons flex">
+                                    <button onClick={() => handleEdit(item.id)} className="btn-primary mx-2">Edit</button>
+                                    <button onClick={() => handleDelete(item.id)} className="btn-primary">Delete</button>
                                 </div>
                             </div>
-                            <div className="buttons flex">
-                                <button onClick={() => handleEdit(item.id)} className="btn-primary mx-2">Edit</button>
-                                <button onClick={() => handleDelete(item.id)} className="btn-primary">Delete</button>
-                            </div>
-                        </div>
-                    })}
+                        })}
                 </div>
             </div>
         </main>
