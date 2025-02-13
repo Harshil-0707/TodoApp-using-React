b81b7e066613f4304245e35fbdd85c2821eb1824
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2025-02-14 01:03:33 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-    return (
-        <nav className="flex justify-around items-center bg-gray-800 text-slate-200 py-2 mb-9 h-20">
-            <h1 className="text-4xl text-center">Manage Your Todo</h1 >
-        </nav>
-    )
-}
+  return (
+    <nav className="flex justify-around items-center bg-gray-800 text-slate-200 py-2 mb-9 h-20">
+      <h1 className="text-4xl text-center">Manage Your Todos</h1>
+    </nav>
+  );
+};
 
 export default Navbar;
